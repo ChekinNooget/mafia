4b5d653170d7b60ec90d824f00564a82f1512b00
ChekinNooget <85902233+ChekinNooget@users.noreply.github.com>
2023-12-26 21:40:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* data: [
+/* scumreadData: [
     user: [
         scumreadwho: [
             awef, awef, awef
@@ -25,12 +25,21 @@ var peopleList = [
 ];
 var currentlyLoadedUser = 0;
 var data;
+var notesData;
 if (localStorage.getItem("scumreads") != null) {
   data = JSON.parse(localStorage.getItem("scumreads"));
 } else {
   data = [];
   for (let i = 0; i < peopleList.length; i++) {
-    data[i] = [];
+    data[i] = [[], []];
+  }
+}
+if (localStorage.getItem("notes") != null) {
+  notesData = JSON.parse(localStorage.getItem("notes"));
+} else {
+  notesData = [];
+  for (let i = 0; i < peopleList.length; i++) {
+    notesData.push("");
   }
 }
 var temp = "";
@@ -46,19 +55,32 @@ clickUser(peopleList[currentlyLoadedUser]);
 
 function addScumreadWho() {
   var scumreadUsers = document.querySelector(".user-info-scumread-who-users");
-  scumreadUsers.appendChild(createDropdown());
-  data[currentlyLoadedUser].push(peopleList[currentlyLoadedUser]);
+  scumreadUsers.appendChild(createScumDropdown());
+  data[currentlyLoadedUser][0].push(peopleList[currentlyLoadedUser]);
   renderNamesInDropdown();
 }
 function removeScumreadWho() {
   var scumreadUsers = document.querySelector(".user-info-scumread-who-users");
   scumreadUsers.removeChild(scumreadUsers.lastChild);
-  data[currentlyLoadedUser].pop();
+  data[currentlyLoadedUser][0].pop();
+  renderNamesInDropdown();
+}
+function addTownreadWho() {
+  var townChildreadUsers = document.querySelector(
+    ".user-info-townread-who-users"
+  );
+  townChildreadUsers.appendChild(createTownDropdown());
+  data[currentlyLoadedUser][1].push(peopleList[currentlyLoadedUser]);
+  renderNamesInDropdown();
+}
+function removeTownreadWho() {
+  var scumreadUsers = document.querySelector(".user-info-townread-who-users");
+  scumreadUsers.removeChild(scumreadUsers.lastChild);
+  data[currentlyLoadedUser][1].pop();
   renderNamesInDropdown();
 }
 function renderNamesInDropdown() {
   //this function remakes all of the options in the selector to refresh it
-  console.log(JSON.stringify(data));
   if (
     document.querySelector(".user-info-scumread-who-users").childElementCount <=
     0
@@ -67,7 +89,27 @@ function renderNamesInDropdown() {
   } else {
     document.querySelector(".user-info-scumread-who-remove").disabled = false;
   }
-  var dropdown = document.querySelectorAll(".user-info-scumread-dropdown");
+  if (
+    document.querySelector(".user-info-townread-who-users").childElementCount <=
+    0
+  ) {
+    document.querySelector(".user-info-townread-who-remove").disabled = true;
+  } else {
+    document.querySelector(".user-info-townread-who-remove").disabled = false;
+  }
+  iterateDropdowns(
+    document.querySelectorAll(".user-info-scumread-dropdown"),
+    0
+  );
+  iterateDropdowns(
+    document.querySelectorAll(".user-info-townread-dropdown"),
+    1
+  );
+
+  saveData();
+}
+
+function iterateDropdowns(dropdown, whatRead) {
   for (let i = 0; i < dropdown.length; i++) {
     var temp = "";
     for (let j = 0; j < peopleList.length; j++) {
@@ -75,8 +117,7 @@ function renderNamesInDropdown() {
         (e) => e !== peopleList[currentlyLoadedUser]
       );
       tempPeopleList.unshift("Choose someone");
-      console.log(JSON.stringify(data));
-      if (data[currentlyLoadedUser][i] == tempPeopleList[j]) {
+      if (data[currentlyLoadedUser][whatRead][i] == tempPeopleList[j]) {
         temp =
           temp +
           `<option value="${tempPeopleList[j]}" selected=true">${tempPeopleList[j]}</option>`;
@@ -88,12 +129,11 @@ function renderNamesInDropdown() {
     }
     dropdown[i].innerHTML = temp;
   }
-  saveData();
 }
 
-function changeDropdown(returnedChild) {
+function changeDropdown(returnedChild, type) {
   var temp = document.querySelector(`.a${returnedChild.toString()}`);
-  data[currentlyLoadedUser][
+  data[currentlyLoadedUser][type][
     Array.prototype.indexOf.call(temp.parentElement.children, temp)
   ] = temp.value; //data equals position of child in parent's children
   renderNamesInDropdown();
@@ -103,17 +143,30 @@ function clickUser(user) {
   currentlyLoadedUser = peopleList.indexOf(user);
   var scumreadUsers = document.querySelector(".user-info-scumread-who-users");
   scumreadUsers.innerHTML = "";
-  for (let i = 0; i < data[currentlyLoadedUser].length; i++) {
-    scumreadUsers.append(createDropdown());
+  for (let i = 0; i < data[currentlyLoadedUser][0].length; i++) {
+    scumreadUsers.append(createScumDropdown());
   }
+  var townreadUsers = document.querySelector(".user-info-townread-who-users");
+  townreadUsers.innerHTML = "";
+  for (let i = 0; i < data[currentlyLoadedUser][1].length; i++) {
+    townreadUsers.append(createTownDropdown());
+  }
+  document.querySelector(".notes-input").value = notesData[currentlyLoadedUser];
+  var userButtons = document.querySelectorAll(".user");
+  for (let i = 0; i < userButtons.length; i++) {
+    userButtons[i].style.backgroundColor = "lightgrey";
+  }
+  userButtons[currentlyLoadedUser].style.backgroundColor = "grey";
   generateScumreadBy();
+  generateTownreadBy();
   renderNamesInDropdown();
 }
+
 function generateScumreadBy() {
   var temp = "";
   for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      if (data[i].includes(peopleList[currentlyLoadedUser])) {
+    for (let j = 0; j < data[i][0].length; j++) {
+      if (data[i][0].includes(peopleList[currentlyLoadedUser])) {
         temp =
           temp + `<li class="user-info-scumread-by-user">${peopleList[i]}</li>`;
       }
@@ -122,19 +175,53 @@ function generateScumreadBy() {
   }
   document.querySelector(".user-info-scumread-by-users").innerHTML = temp;
 }
+
+function generateTownreadBy() {
+  var temp = "";
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i][1].length; j++) {
+      if (data[i][1].includes(peopleList[currentlyLoadedUser])) {
+        temp =
+          temp + `<li class="user-info-townread-by-user">${peopleList[i]}</li>`;
+      }
+      break;
+    }
+  }
+  document.querySelector(".user-info-townread-by-users").innerHTML = temp;
+}
+
 function saveData() {
   localStorage.setItem("scumreads", JSON.stringify(data));
 }
 
-function createDropdown() {
+function createScumDropdown() {
   var scumChild = document.createElement("select");
   var trash = document.querySelector(".trash");
   var temp = Math.floor(Math.random() * 9999);
   trash.appendChild(scumChild);
-  scumChild.outerHTML = `<select class="user-info-scumread-dropdown a${temp}" name="user-info-scumread-dropdown" onchange="changeDropdown(${temp})"></select>`;
+  scumChild.outerHTML = `<select class="user-info-scumread-dropdown a${temp}" name="user-info-scumread-dropdown" onchange="changeDropdown(${temp}, 0)"></select>`;
   try {
     return trash.firstChild;
   } finally {
     trash.innerHTML = "";
   }
+}
+function createTownDropdown() {
+  var townChild = document.createElement("select");
+  var trash = document.querySelector(".trash");
+  var temp = Math.floor(Math.random() * 9999);
+  trash.appendChild(townChild);
+  townChild.outerHTML = `<select class="user-info-townread-dropdown a${temp}" name="user-info-townread-dropdown" onchange="changeDropdown(${temp}, 1)"></select>`;
+  try {
+    return trash.firstChild;
+  } finally {
+    trash.innerHTML = "";
+  }
+}
+
+/* NOTESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS */
+
+function notesChange() {
+  notesData[currentlyLoadedUser] = document.querySelector(".notes-input").value;
+  localStorage.setItem("notes", JSON.stringify(notesData));
 }
