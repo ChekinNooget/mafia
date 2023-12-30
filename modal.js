109b8a553b0b3562e8c71577347b16445ec4da3e
ChekinNooget <85902233+ChekinNooget@users.noreply.github.com>
2023-12-29 19:52:02 -0700

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,29 +1,39 @@
-var modal = document.querySelector(".userlist-modal");
+var dataModal = document.querySelector(".userlist-modal");
+var fileModal = document.querySelector(".files-modal");
 var overlay = document.querySelector(".overlay");
-var openModalBtn = document.querySelector(".btn-open");
-var closeModalBtn = document.querySelector(".btn-close");
+var openDataBtn = document.querySelector(".data-open");
+var openFileBtn = document.querySelector(".file-open");
+var closeModalBtn = document.querySelectorAll(".btn-close");
 
 // close modal function
 const closeModal = function () {
-  modal.classList.add("hidden");
+  dataModal.classList.add("hidden");
+  fileModal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
 // close the modal when the close button and overlay is clicked
-closeModalBtn.addEventListener("click", closeModal);
+for (let i = 0; i < closeModalBtn.length; i++) {
+  closeModalBtn[i].addEventListener("click", closeModal);
+}
 overlay.addEventListener("click", closeModal);
 
 // close modal when the Esc key is pressed
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+  if (e.key === "Escape" && !dataModal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
 // open modal function
 const openModal = function () {
-  modal.classList.remove("hidden");
+  dataModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}; // open modal function
+const openFile = function () {
+  fileModal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 // open modal event
-openModalBtn.addEventListener("click", openModal);
+openDataBtn.addEventListener("click", openModal);
+openFileBtn.addEventListener("click", openFile);
